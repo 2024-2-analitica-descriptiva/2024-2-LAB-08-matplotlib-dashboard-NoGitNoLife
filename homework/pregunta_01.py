9ce1690bfd5e77db115940032087a8d5a1b93ef0
NoGitNoLife <111093017+NoGitNoLife@users.noreply.github.com>
2025-01-23 20:01:15 -0500

    prev

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -1,3 +1,22 @@
 # pylint: disable=line-too-long
 def pregunta_01():
     return
+
+import matplotlib.pyplot as plr
+import pandas as pd 
+
+csvloc = "../files/input/shipping-data.csv"
+
+def load_data():
+    df = pd.read_csv(csvloc)
+    return df
+
+dataro = load_data()
+
+print (dataro.head())
+
+"""
+* El archivo de datos se encuentra en la carpeta `data`.
+* Todos los archivos debe ser creados en la carpeta `docs`.
+* Su código debe crear la carpeta `docs` si no existe.
+"""
